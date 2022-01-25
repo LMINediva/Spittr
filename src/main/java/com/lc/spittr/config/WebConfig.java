caeff7b49887cf042a257a6ac52a4fd2b1a78aa6
Lezoc163 <3037907347@qq,com>
2022-01-25 16:52:33 +0800

    使用Spring通用标签库展现国际化信息

--- src/main/java/com/lc/spittr/config/WebConfig.java
+++ src/main/java/com/lc/spittr/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.lc.spittr.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -31,5 +33,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void configureDefaultServletHandling(
             DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setCacheSeconds(10);
+        return messageSource;
     }
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,6 @@
 <%@page session="false" pageEncoding="UTF-8" %>
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@taglib uri="http://www.springframework.org/tags" prefix="s" %>
 <!DOCTYPE html>
 <html lang="zh">
 <head>
@@ -8,7 +9,7 @@
     <link rel="stylesheet" href="<c:url value="/resources/style.css"/>">
 </head>
 <body>
-<h1>欢迎来到足迹微博</h1>
+<h1><s:message code="spittr.welcome"/></h1>
 <a href="<c:url value="/"/>">足迹博客</a>
 <a href="<c:url value="https://www.baidu.com"/>">注册</a>
 </body>
